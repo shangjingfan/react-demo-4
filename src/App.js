2e7053fc65769464cc3e77887498a072316dec13
shangjingfan <876625909@qq.com>
2021-02-14 22:35:37 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+const App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div>App组件</div> 相当于React.createElement()
+    <div>App组件
+      <Component numbers={[1,2,3,4]}/>
     </div>
-  );
+    // React.createElement("div", null, "App组件")
+  )
 }
-
+const Component = (props) =>{
+  const array = []
+  for(let i=0; i<props.numbers.length; i++){
+    array.push(<div>下标为{i}，值为{props.numbers[i]}</div>)
+  }
+  return ( <div>{ array }</div> )
+}
+/* 上面并不是标准JS的写法 */
 export default App;
