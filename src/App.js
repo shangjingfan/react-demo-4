4b891bfdd2a18a0c634ff9ef5d920af3fe27bcbf
shangjingfan <876625909@qq.com>
2021-03-01 10:54:48 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-const App = ()=>{
+
+const App = () => {
   return (
     // <div>App组件</div> 相当于React.createElement()
     <div>App组件
-      <Component numbers={[1,2,3,4]}/>
+      <Component numbers={[1, 2, 3, 4]} />
     </div>
     // React.createElement("div", null, "App组件")
   )
 }
-const Component = (props) =>{
+const Component = (props) => {
   const array = []
-  for(let i=0; i<props.numbers.length; i++){
+  for (let i = 0; i < props.numbers.length; i++) {
     array.push(<div>下标为{i}，值为{props.numbers[i]}</div>)
   }
-  return ( <div>{ array }</div> )
+  return (<div>{array}</div>)
 }
 /* 上面并不是标准JS的写法 */
 export default App;
+
